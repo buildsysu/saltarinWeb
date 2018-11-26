7dce488481657c46f7fc4e565284ff05eabfd839
IkaruTak <isc.fernandowg@gmail.com>
2018-11-26 02:24:55 -0600

    Se hacen modificaciones ára la visualización de perfiles.

--- user/profile.php
+++ user/profile.php
@@ -6,7 +6,11 @@ require_once '../inc/controllers/ProfilePictureDto.php';
 
 $uDto = new UserDto();
 $ppDto = new ProfilePictureDto();
-$idUser = $uDto->getIdUser($_SESSION['username']);
+$edition = false;
+if ($_GET['user'] == $_SESSION['username']) {
+    $edition = true;
+}
+$idUser = $uDto->getIdUser($_GET['user']);
 $user = $uDto->read($idUser);
 $purl = $ppDto->read($user['fk_idProfilePicture']);
 
@@ -18,24 +22,47 @@ if ($purl != false) {
 
 $errores = '';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_FILES)) {
-    $check = @getimagesize($_FILES['photo']['tmp_name']);
-    if ($check != false) {
-        $destiny = '../img/user/';
-        $rand = substr(md5(microtime()),rand(0,26),5);
-        $file_upload = $destiny . $rand . $_FILES['photo']['name'];
-        move_uploaded_file($_FILES['photo']['tmp_name'], $file_upload);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-        $idPp = $ppDto->create($rand . $_FILES['photo']['name']);
-        $uDto->updateProfilePicture($idPp,$idUser);
+    if(!empty($_FILES)) {
+        $check = @getimagesize($_FILES['photo']['tmp_name']);
+        if ($check != false) {
+            $rand = substr(md5(microtime()),rand(0,26),10);
+            $file_upload = '../img/user/' . $rand;
+            move_uploaded_file($_FILES['photo']['tmp_name'], $file_upload);
 
-        $_SERVER['REQUEST_METHOD'] = null;
-        $_FILES = null;
-
-        header('Location: profile.php');
-    } else {
-        $errores .= "No es un formato de imagen válido.";
+            $idPp = $ppDto->create($rand);
+            $uDto->updateProfilePicture($idPp,$idUser);
+        } else {
+            $errores .= "No es un formato de imagen válido.";
+        }
     }
+
+    $lastpass = sha1($_POST['lastpass']);
+    $password = sha1($_POST['password']);
+    if(!empty($lastpass) && !empty($password)) {
+        if($user['password'] == $lastpass) {
+            $uDto->updatePassword($idUser,$password);
+        } else {
+            $errores .= "La contraseña actual no coincide.";
+        }
+    } else {
+        $errores .= "Por favor rellene todos los campos.";
+    }
+
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+    $phone = filter_var($_POST['phone'], FILTER_SANITIZE_STRING);
+    if(!empty($email) && !empty($phone)) {
+        $uDto->updateInfo($idUser,$email,$phone);
+    } else {
+        $errores .= "Por favor rellene todos los campos.";
+    }
+
+    if ($errores == '') {
+        header('Location: profile.php?user='.$_SESSION['username']);
+        // echo "Llegue al final";
+    }
+
 }
 
 require '../view/user/profile.view.php';
